a651f331274d1fa5fd1d788776100908f823129c
Vincent Xu <vincent.hao.xu@gmail.com>
2021-11-30 21:56:17 +0800

    module should bind implementation to type

--- cdi/src/main/kotlin/Module.kt
+++ cdi/src/main/kotlin/Module.kt
@@ -1,11 +1,20 @@
 package firenze.cdi
 
+import javax.inject.Provider
+
 class Module {
-    private val components = HashMap<Class<*>, Any>()
+    private val components = HashMap<Class<*>, Provider<*>>()
 
     fun <T> bind(type: Class<T>, instance: T) {
-        components[type] = instance!!
+        components[type] = Provider { instance }
     }
 
-    fun <T> get(type: Class<T>): T = components[type] as T
+    fun <T> bind(type: Class<T>, implementationClass: Class<out T>) {
+        components[type] = Provider {
+            val defaultConstructor = implementationClass.getConstructor()
+            defaultConstructor.newInstance()
+        }
+    }
+
+    fun <T> get(type: Class<T>): T = components[type]!!.get() as T
 }
--- cdi/src/test/kotlin/ModuleTest.kt
+++ cdi/src/test/kotlin/ModuleTest.kt
@@ -2,11 +2,12 @@ package firenze.cdi
 
 import kotlin.test.Test
 import kotlin.test.assertSame
+import kotlin.test.assertTrue
 
 class ModuleTest {
 
     @Test
-    fun should_bind_instance_to_interface(){
+    fun should_bind_instance_to_type(){
         val module = Module()
         val component = object: Component {}
 
@@ -15,5 +16,16 @@ class ModuleTest {
         assertSame(component, module.get(Component::class.java))
     }
 
+    @Test
+    fun should_bind_implement_class_to_type() {
+        val module = Module()
+
+        module.bind(Component::class.java, Implementation::class.java)
+
+        assertTrue(module.get(Component::class.java) is Implementation)
+    }
+
     interface Component
+
+    class Implementation : Component
 }
